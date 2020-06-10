cb2d4fd8f17e581fb81ad5718708e43974cce045
tangyaoxiang <2389092255@qq.com>
2020-06-11 02:14:12 +0800

    add abtractFacotyr

--- AbtractFactory/Program.cs
+++ AbtractFactory/Program.cs
@@ -1,12 +1,149 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+/// <summary>
+/// 抽像工厂
+/// </summary>
+namespace AbtractFactory {
+    class Program {
+        static void Main (string[] args) {
+            //IFactory factory = new SqlServerFacotory ();
 
-namespace AbtractFactory
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+            //IUser user = factory.CreateUser ();
+            IUser user = DataAccess.CreateUser ();
+            user.GetUsers (0);
+            user.Insert (new Users ());
+
+            //IDepartment department = factory.CreateDepartment ();
+            IDepartment department = DataAccess.CreateDepartment ();
+            department.Insert (new Department ());
+            department.GetDepartment (0);
+
+            System.Console.Read ();
         }
     }
+
+    class DataAccess {
+        //读取NETCORE 配置文件暂时有问题
+        // static IConfigurationRoot config = new ConfigurationBuilder ()
+        //     .SetBasePath (Directory.GetCurrentDirectory ())
+        //     .AddJsonFile ("AbtractFactory/appsettings.json", optional : true, reloadOnChange : false)
+        //     .AddEnvironmentVariables ()
+        //     .Build ();
+
+        static readonly string assemblyName = "AbtractFactory";
+        /// <summary>
+        /// 暂时写死类型
+        /// </summary>
+        static string className = assemblyName + "." +"Access";// config.GetSection ("db:dbName").Value;
+        /// <summary>
+        /// 获取程序集根目录
+        /// </summary>
+        /// <returns></returns>
+        static string binPath = Path.GetDirectoryName (Assembly.GetEntryAssembly ().Location);
+        public static IUser CreateUser () {
+            Assembly ass = Assembly.Load (new AssemblyName (assemblyName));
+            Type type = ass.GetType (className + "User");
+            return (IUser) Activator.CreateInstance (type);
+        }
+        public static IDepartment CreateDepartment () {
+            Assembly ass = Assembly.Load (new AssemblyName (assemblyName));
+            Type type = ass.GetType (className + "Department");
+            return (IDepartment) Activator.CreateInstance (type);
+        }
+    }
+
+    #region  Interface
+    public interface IFactory {
+        IUser CreateUser ();
+        IDepartment CreateDepartment ();
+    }
+    public interface IDepartment {
+        void Insert (Department department);
+        Department GetDepartment (int id);
+    }
+    public interface IUser {
+        void Insert (Users user);
+        Users GetUsers (int id);
+    }
+    #endregion
+
+    #region Sqlserver
+
+    public class SqlServerDepartment : IDepartment {
+        public Department GetDepartment (int id) {
+            System.Console.WriteLine ("sqlserver get department by id");
+            return null;
+        }
+
+        public void Insert (Department department) {
+            System.Console.WriteLine ("sqlserver insert department");
+        }
+    }
+    public class SqlServerUser : IUser {
+        public Users GetUsers (int id) {
+            System.Console.WriteLine ("sqlserver get Users by id");
+            return null;
+        }
+
+        public void Insert (Users user) {
+            System.Console.WriteLine ("sqlserver insert users");
+        }
+    }
+    public class SqlServerFacotory : IFactory {
+        public IDepartment CreateDepartment () {
+            return new SqlServerDepartment ();
+        }
+
+        public IUser CreateUser () {
+            return new SqlServerUser ();
+        }
+    }
+    #endregion
+
+    #region Access
+    public class AccessDepartment : IDepartment {
+        public Department GetDepartment (int id) {
+            System.Console.WriteLine ("access get department by id");
+            return null;
+        }
+
+        public void Insert (Department department) {
+            System.Console.WriteLine ("access insert department");
+        }
+    }
+    public class AccessUser : IUser {
+        public Users GetUsers (int id) {
+            System.Console.WriteLine ("access get Users by id");
+            return null;
+        }
+
+        public void Insert (Users user) {
+            System.Console.WriteLine ("access insert users");
+        }
+    }
+    public class AccessFacotyr : IFactory {
+        public IDepartment CreateDepartment () {
+            return new AccessDepartment ();
+        }
+        public IUser CreateUser () {
+            return new AccessUser ();
+        }
+    }
+    #endregion
+
+    #region Model
+    public class Department {
+        public int ID { get; set; }
+        public string DepartmentName { get; set; }
+        public List<Users> User { get; set; }
+    }
+
+    public class Users {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+    }
+    #endregion
 }
--- Proxy/Program.cs
+++ Proxy/Program.cs
@@ -2,13 +2,37 @@
 /// <summary>
 /// 代理模式
 /// </summary>
-namespace proxy
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            
+namespace proxy {
+    class Program {
+        static void Main (string[] args) {
+            Porxy proxy = new Porxy ();
+            proxy.Gift ();
+        }
+    }
+
+    /// <summary>
+    /// 代理接口
+    /// </summary>
+    public interface IProxy {
+        void Gift ();
+    }
+
+    /// <summary>
+    /// 代理类
+    /// </summary>
+    public class Porxy : IProxy {
+        Pursirt _pursirt = null;
+        public void Gift () {
+            _pursirt = new Pursirt ();
+            _pursirt.Gift ();
+        }
+    }
+    /// <summary>
+    /// 实际请求类
+    /// </summary>
+    public class Pursirt : IProxy {
+        public void Gift () {
+            System.Console.WriteLine ("gift");
         }
     }
 }
