38217d284b11c80e61e0ff56ea001c590a400104
唐耀祥 <tangyaoxiang@tangyaoxiangdeMacBook-Pro.local>
2020-06-10 15:13:16 +0800

    desingeModel first commit

---
+++ Decorate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Decorate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
