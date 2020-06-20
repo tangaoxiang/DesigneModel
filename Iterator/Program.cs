1c0c217fca7e9df0b3c6f94cc394f8ba6a306ae8
tangyaoxiang <2389092255@qq.com>
2020-06-20 22:54:40 +0800

    commit Singleton

--- Iterator/Program.cs
+++ Iterator/Program.cs
@@ -1,12 +1,97 @@
 ﻿using System;
+using System.Collections.Generic;
+/// <summary>
+/// Iterator 迭代器模式 Foreach
+/// </summary>
+namespace Iterator {
+    class Program {
+        static void Main (string[] args) {
+            ConcreteAggregate a = new ConcreteAggregate ();
+            a[0] = "孙悟空";
+            a[1] = "猪八戒";
+            a[2] = "沙僧";
+            a[3] = "白骨精";
+            a[4] = "玉帝";
 
-namespace Iterator
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+            Iterator iterator = new ConcreteIterator (a);
+            object item = iterator.First ();
+            while (!iterator.IsDone ()) {
+                System.Console.WriteLine ($"{iterator.CurrentItem()}");
+                iterator.Next ();
+            }
+        }
+    }
+    /// <summary>
+    /// 迭代器抽像类
+    /// </summary>
+    abstract class Iterator {
+        public abstract object First ();
+        public abstract object Next ();
+        public abstract bool IsDone ();
+        public abstract object CurrentItem ();
+    }
+
+    /// <summary>
+    /// 聚集抽像类
+    /// </summary>
+    abstract class Aggregate {
+        /// <summary>
+        /// 创建迭代器
+        /// </summary>
+        /// <returns></returns>
+        public abstract Iterator CreateIterator ();
+    }
+    /// <summary>
+    /// 具体聚集类
+    /// </summary>
+    class ConcreteAggregate : Aggregate {
+        IList<object> items = new List<object> ();
+
+        public override Iterator CreateIterator () {
+            return new ConcreteIterator (this);
+        }
+        /// <summary>
+        /// 返回聚集个
+        /// </summary>
+        /// <value></value>
+        public int Count { get { return items.Count; } }
+        /// <summary>
+        /// 索引器
+        /// </summary>
+        /// <value></value>
+        public object this [int index] {
+            get { return items[index]; }
+            set { items.Insert(index, value); }
+        }
+    }
+    /// <summary>
+    /// 具体迭代器
+    /// </summary>
+    class ConcreteIterator : Iterator {
+        private ConcreteAggregate aggregate;
+        private int current = 0;
+        public ConcreteIterator (ConcreteAggregate aggregate) {
+            this.aggregate = aggregate;
+        }
+        public override object CurrentItem () {
+            return aggregate[current];
+        }
+
+        public override object First () {
+            return aggregate[0];
+        }
+
+        public override bool IsDone () {
+            return current >= aggregate.Count?true : false;
+        }
+
+        public override object Next () {
+            object ret = null;
+            current++;
+            if (current < aggregate.Count) {
+                ret = aggregate[current];
+            }
+            return ret;
         }
     }
 }
---
+++ Singleton/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+/// <summary>
+/// 单例模式
+/// </summary>
+namespace Singleton {
+    class Program {
+        static void Main (string[] args) {
+            Singleton.GetInstance ().Test ();
+            Singleton2.GetInstance().Test2();
+        }
+    }
+    /// <summary>
+    /// 懒汉式单例
+    /// </summary>
+    class Singleton {
+        static Singleton instance;
+        static readonly object synObj = new object ();
+        private Singleton () { }
+        public static Singleton GetInstance () {
+            if (instance == null) {
+                lock (synObj) {
+                    if (instance == null) {
+                        instance = new Singleton ();
+                    }
+                }
+            }
+            return instance;
+        }
+
+        public void Test () {
+            System.Console.WriteLine ("test success");
+        }
+    }
+    /// <summary>
+    /// 饿汉式单例
+    /// </summary>
+    sealed class Singleton2 {
+
+        /// <summary>
+        /// 依赖.NET运行库初始化
+        /// 静态初始化在自己被加载时就将自己实例化
+        /// </summary>
+        /// <returns></returns>
+        static readonly Singleton2 instance = new Singleton2 ();
+        private Singleton2 () { }
+        public static Singleton2 GetInstance () {
+            return instance;
+        }
+
+        public void Test2 () {
+            System.Console.WriteLine ("test2 success");
+        }
+    }
+}
