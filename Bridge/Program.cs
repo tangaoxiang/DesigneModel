26e0811ad2b75bb27384fa08f2c44c91aae44b2d
tangyaoxiang <2389092255@qq.com>
2020-06-23 01:23:17 +0800

    commit Bridge

---
+++ Bridge/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+/// <summary>
+/// 桥接模式
+/// 将抽象部分与它的实现部分分离，使它们都可能独立地变化
+/// 并非让抽象和派生分享，实现指的是抽象类和它的派生类来实现自己的对象
+/// </summary>
+namespace Bridge {
+    class Program {
+        static void Main (string[] args) {
+            Abstraction abs = new RefindAbstraction ();
+            abs.SetImplementor (new ConcreteImplementorA ());
+            abs.Operation ();
+
+            abs.SetImplementor (new ConcreteImplementorB ());
+            abs.Operation ();
+        }
+    }
+
+    abstract class Implementor {
+        public abstract void Operation ();
+    }
+    class ConcreteImplementorA : Implementor {
+        public override void Operation () {
+            System.Console.WriteLine ("A");
+        }
+    }
+    class ConcreteImplementorB : Implementor {
+        public override void Operation () {
+            System.Console.WriteLine ("B");
+        }
+    }
+
+    class Abstraction {
+        protected Implementor implementor;
+        public void SetImplementor (Implementor implementor) {
+            this.implementor = implementor;
+        }
+        public virtual void Operation () {
+            implementor.Operation ();
+        }
+    }
+
+    class RefindAbstraction : Abstraction {
+        public override void Operation () {
+            implementor.Operation ();
+        }
+    }
+}
